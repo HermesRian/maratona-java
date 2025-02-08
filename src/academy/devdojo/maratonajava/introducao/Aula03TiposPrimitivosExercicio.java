3fd7d3eafd76702d927c8c3087db61cf6673947b
HermesRian <rianfoz@hotmail.com>
2025-02-08 08:04:12 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+
+    public static void main(String[] args) {
+        String nome, endereco, data;
+        double salario;
+
+        nome = "Rian";
+        endereco = "Rua vivaldo de lima, 1720";
+        salario = 2000.00;
+        data = "01/03/2025";
+        System.out.println("Eu, "+nome+ ", morando no endereço " +endereco+", confirmo que recebi o salário de " +salario+ ", na data " +data+".");
+    }
+}
