237292c6f81d51f6227b6293d6f94a70bd3b361c
HermesRian <rianfoz@hotmail.com>
2025-02-11 08:15:45 -0300

    Create Aula02TiposPrimitivos.java

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        double salario = 3000;
+        double valorPlay5 = 2500;
+        String isPlay5Compravel = salario >= valorPlay5 ? "Dá pra comprar!" : "Junta mais um pouco.";
+        System.out.println(isPlay5Compravel);
+    }
+}
