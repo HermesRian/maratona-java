1307bb83d1806f88ed0e271b3046b617d839b893
HermesRian <rianfoz@hotmail.com>
2025-02-12 01:56:32 -0300

    Create Aula02TiposPrimitivos.java

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 10) {
+            System.out.println("While: "+i);
+            i++;
+        }
+        System.out.println("");
+
+        for (i=0;i<=10;i++){
+            System.out.println("For: "+i);
+        }
+    }
+}
