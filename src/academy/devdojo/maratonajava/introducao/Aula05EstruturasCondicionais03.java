a831bdcc545f67d1211ca715fc56b0412e9364e6
HermesRian <rianfoz@hotmail.com>
2025-02-14 05:10:44 -0300

    Create Aula02TiposPrimitivos.java

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -1,8 +1,12 @@
 package academy.devdojo.maratonajava.introducao;
 
+import java.util.Scanner;
+
 public class Aula05EstruturasCondicionais03 {
     public static void main(String[] args) {
-        double salario = 3000;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entre com o salário: ");
+        double salario = scanner.nextDouble();
         double valorPlay5 = 2500;
         String isPlay5Compravel = salario >= valorPlay5 ? "Dá pra comprar!" : "Junta mais um pouco.";
         System.out.println(isPlay5Compravel);
