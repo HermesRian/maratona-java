2bcade45ad5627b468e86967a74b0d07f14698f8
HermesRian <rianfoz@hotmail.com>
2025-02-15 21:13:24 -0300

    Create Aula02TiposPrimitivos.java

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays03 {
+    public static void main(String[] args) {
+        int[] idades = {10, 15, 20, 25};
+
+        for (int i : idades){
+            System.out.println(i);
+        }
+    }
+}
