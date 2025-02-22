f03baecca1be966d4c962b80a222249a32c72260
HermesRian <rianfoz@hotmail.com>
2025-02-21 22:41:43 -0300

    .

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -9,14 +9,14 @@ public class CarroTest01 {
         carro1.modelo = "G63";
         carro1.ano = 2022;
 
-        System.out.println("Marca: "+ carro1.nome+", modelo: "+ carro1.modelo+", ano: "+ carro1.ano);
+        System.out.println("Marca: " + carro1.nome + ", modelo: " + carro1.modelo + ", ano: " + carro1.ano);
 
         Carro carro2 = new Carro();
         carro2.nome = "Porsche";
         carro2.modelo = "911";
         carro2.ano = 2023;
-
-        System.out.println("Marca: "+ carro2.nome+", modelo: "+ carro2.modelo+", ano: "+ carro2.ano);
+     //   carro2 = carro1; //trocando a referencia do objeto
+        System.out.println("Marca: " + carro2.nome + ", modelo: " + carro2.modelo + ", ano: " + carro2.ano);
     }
 
 
