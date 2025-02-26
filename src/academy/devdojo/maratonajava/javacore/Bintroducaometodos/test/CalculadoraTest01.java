d84db73e2e11cc93de26b59a9d8dfaad402bb6a6
HermesRian <rianfoz@hotmail.com>
2025-02-25 23:39:21 -0300

    .

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -5,7 +5,6 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculad
 public class CalculadoraTest01 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-
         calculadora.somaDoisNumeros();
         calculadora.subtraiDoisNumeros();
     }
