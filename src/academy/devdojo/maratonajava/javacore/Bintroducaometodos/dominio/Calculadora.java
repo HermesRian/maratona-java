5b19a95f086bd9c33645be3255ab8e275e91d935
HermesRian <rianfoz@hotmail.com>
2025-02-26 23:56:52 -0300

    .

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -4,7 +4,12 @@ public class Calculadora {
     public void somaDoisNumeros() {
         System.out.println(5 + 5);
     }
+
     public void subtraiDoisNumeros() {
         System.out.println(10 - 5);
+    }
+
+    public void multiplicaDoisNumeros(int num1, int num2) {
+        System.out.println(num1 * num2);
     }
 }
