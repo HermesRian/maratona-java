68d2ddc0756ad8e221f9440fe8bff3ce9a6bc52f
HermesRian <rianfoz@hotmail.com>
2025-02-27 23:40:28 -0300

    .

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -5,7 +5,6 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculad
 public class CalculadoraTest02 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-
         calculadora.multiplicaDoisNumeros(2, 5);
     }
 }
